571b7bbf3e276d4e3d578c6665d845ac36f15f10
Vaibhav Mishra <mkrrish575@gmail.com>
2026-08-05 08:45:50 +0000

    testing reading UI

---
+++ src/reading-main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ReadingSection from './ReadingSection.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('reading-root'));
+root.render(
+  <React.StrictMode>
+    <ReadingSection />
+  </React.StrictMode>
+);
